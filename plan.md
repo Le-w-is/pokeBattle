895e80b55e2557e1768f2519255d458e1762894d
Lewis <100314560+Le-w-is@users.noreply.github.com>
2023-01-25 13:57:22 +0000

    displaying scores and results

--- plan.md
+++ plan.md
@@ -10,8 +10,23 @@
 //1d. attach the variable to the pokeapi url ✅
 
 
+//fight scores ✅
+// add up all the numbers and divide by 6 = total power
+// compare that to computers total power
+// declare a winner
+
+
+
+
+//competition
+// allow the player to play six times (a full team in pokemon)
+// tally up the wins and losses on each round to provide a total
+// declare the winner after six rounds and reset the scores
+
+
+
 // CSS plan
-// put the poke pic and stats into a nice looking card
+// put the poke pic and stats into a card ✅ 
 
 
-// make a fight button
+
--- scripts.js
+++ scripts.js
@@ -1,18 +1,38 @@
 // api url
-"https://pokeapi.co/api/v2/pokemon/{id or name}/"
-
+"https://pokeapi.co/api/v2/pokemon/{id or name}/";
 
 // DEFINEING VARIABLES
-let sprite = document.querySelector("#sprite")
-let pokeName = document.querySelector("#pokeName")
-let attack = document.querySelector("#attack")
-let defense = document.querySelector("#defense")
-let speed = document.querySelector("#speed")
-let specialAttack = document.querySelector("#specialAttack")
-let specialDefense = document.querySelector("#specialDefense")
-let playerPokemonChoice = document.querySelector("#pokemonChoice")
-let playerChoice = Math.floor(Math.random() * 1009);
+
+//display variables
+let playerSprite = document.querySelector("#playerSprite");
+let computerSprite = document.querySelector("#computerSprite");
+let playerPokeName = document.querySelector("#playerPokeName");
+let playerAttack = document.querySelector("#playerAttack");
+let playerDefense = document.querySelector("#playerDefense");
+let playerSpeed = document.querySelector("#playerSpeed");
+let playerSpecialAttack = document.querySelector("#playerSpecialAttack");
+let playerSpecialDefense = document.querySelector("#playerSpecialDefense");
+
+let computerPokeName = document.querySelector("#computerPokeName");
+let computerAttack = document.querySelector("#computerAttack");
+let computerDefense = document.querySelector("#computerDefense");
+let computerSpeed = document.querySelector("#computerSpeed");
+let computerSpecialAttack = document.querySelector("#computerSpecialAttack");
+let computerSpecialDefense = document.querySelector("#computerSpecialDefense");
+let computerPokemonChoice = document.querySelector("#computerPokemonChoice");
+
+//choice variables
+let playerPokemonChoice = document.querySelector("#playerPokemonChoice");
 let computerChoice = Math.floor(Math.random() * 1009);
+
+
+// result variables
+let totalPlayerPower = document.querySelector("#totalPlayerPower")
+let totalComputerPower = document.querySelector("#totalComputerPower")
+let result = document.querySelector("#result");
+let totals = document.querySelector("#totals")
+let win = 0;
+let lose = 0;
 
 
 //EVENT LISTENERS
@@ -20,33 +40,73 @@ playerPokemonChoice.addEventListener(`keydown`, handlePlayerPokemon);
 
 
 //FUNCTIONS
+
+//function creates the fight when the player presses enter
 function handlePlayerPokemon(e) {
     if (e.key == `Enter`) {
         playerChoice = playerPokemonChoice.value;
+        computerChoice = Math.floor(Math.random() * 1009)
+        displayPlayerPokemon(playerChoice);
+        displayComputerPokemon(computerChoice);
+        fight();
     }
-    displayPokemon(playerChoice)
-}
+};
 
+
+// fetch function to get the promise from the api and set the date that is needed to a variable
 async function getPokemonData(pokeRef) {
     let pokeURL = `https://pokeapi.co/api/v2/pokemon/${pokeRef}/`;
-    const res = await fetch(pokeURL)
+    const res = await fetch(pokeURL);
     data = await res.json();
-    const pokeData = { name: data.forms[0].name, hp: data.stats[0].base_stat, attack: data.stats[1].base_stat, defense: data.stats[2].base_stat, specialAttack: data.stats[3].base_stat, specialDefense: data.stats[4].base_stat, speed: data.stats[5].base_stat, sprite: data.sprites.front_shiny }
-    return pokeData
-}
+    const pokeData = { name: data.forms[0].name, hp: data.stats[0].base_stat, attack: data.stats[1].base_stat, defense: data.stats[2].base_stat, specialAttack: data.stats[3].base_stat, specialDefense: data.stats[4].base_stat, speed: data.stats[5].base_stat, sprite: data.sprites.front_shiny };
+    return pokeData;
+};
 
 
-async function displayPokemon() {
-    let data = await getPokemonData(playerChoice)
-    sprite.src = data.sprite;
-    pokeName.innerHTML = data.name;
-    attack.innerHTML = `Attack: ${data.attack}`
-    defense.innerHTML = `Defense: ${data.defense}`
-    speed.innerHTML = `Speed: ${data.speed}`
-    specialAttack.innerHTML = `Special Attack: ${data.specialAttack}`
-    specialDefense.innerHTML = `Special Defense: ${data.specialDefense}`
-}
+// Player function unpacks the data and assigns it to the variables linked to html divs 
+async function displayPlayerPokemon() {
+    let data = await getPokemonData(playerChoice);
+    playerSprite.src = data.sprite;
+    playerPokeName.innerHTML = data.name;
+    playerAttack.innerHTML = `Attack: ${data.attack}`;
+    playerDefense.innerHTML = `Defense: ${data.defense}`;
+    playerSpeed.innerHTML = `Speed: ${data.speed}`;
+    playerSpecialAttack.innerHTML = `Special Attack: ${data.specialAttack}`;
+    playerSpecialDefense.innerHTML = `Special Defense: ${data.specialDefense}`;
+};
 
-displayPokemon(playerChoice)
+// Computer function unpacks the data and assigns it to the variables linked to html divs 
+async function displayComputerPokemon() {
+    let data = await getPokemonData(computerChoice);
+    computerSprite.src = data.sprite;
+    computerPokeName.innerHTML = data.name;
+    computerAttack.innerHTML = `Attack: ${data.attack}`;
+    computerDefense.innerHTML = `Defense: ${data.defense}`;
+    computerSpeed.innerHTML = `Speed: ${data.speed}`;
+    computerSpecialAttack.innerHTML = `Special Attack: ${data.specialAttack}`;
+    computerSpecialDefense.innerHTML = `Special Defense: ${data.specialDefense}`;
+};
 
+
+// function that tallies up the total score for each pokemon, compares the two, then provides the result
+
+async function fight() {
+    let playerData = await getPokemonData(playerChoice);
+    let computerData = await getPokemonData(computerChoice);
+    let playerScore = playerData.hp + playerData.attack + playerData.defense + playerData.specialAttack + playerData.specialDefense + playerData.speed
+    let computerScore = computerData.hp + computerData.attack + computerData.defense + computerData.specialAttack + computerData.specialDefense + computerData.speed
+
+
+    totalPlayerPower.innerHTML = `your player score is ${playerScore}     `;
+    totalComputerPower.innerHTML = `the computer score is ${computerScore}     `;
+
+
+    if (playerScore >= computerScore) {
+        result.innerHTML = "win"
+        win++
+    } else {
+        result.innerHTML = "lose"
+        lose++
+    }
+};
 
