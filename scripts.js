ecea57ca7de4785a46d92a6070e2615d86440507
Lewis <100314560+Le-w-is@users.noreply.github.com>
2023-01-25 10:54:40 +0000

    pokemon sprite displaying on screen

--- scripts.js
+++ scripts.js
@@ -2,21 +2,26 @@
 "https://pokeapi.co/api/v2/pokemon/{id or name}/"
 
 let pokeRef = 1
-
+let sprite = document.querySelector("#sprite")
+console.log(sprite)
 
 async function getPokemonData(pokeRef) {
     let pokeURL = `https://pokeapi.co/api/v2/pokemon/${pokeRef}/`;
-    const response = await fetch(pokeURL);
-    let data = await response.json();
-    console.log(data.forms[0].name, data.stats[0].stat.name, data.stats[0].base_stat, data.stats[1].stat.name, data.stats[1].base_stat,
-        data.stats[2].stat.name, data.stats[2].base_stat,
-        data.stats[3].stat.name, data.stats[3].base_stat,
-        data.stats[4].stat.name, data.stats[4].base_stat,
-        data.stats[5].stat.name, data.stats[5].base_stat,
-    )
-    return data
+    const res = await fetch(pokeURL)
+    data = await res.json();
+    const pokeData = { name: data.forms[0].name, hp: data.stats[0].base_stat, attack: data.stats[1].base_stat, defense: data.stats[2].base_stat, specialAttack: data.stats[3].base_stat, specialDefense: data.stats[4].base_stat, speed: data.stats[5].base_stat, sprite: data.sprites.front_shiny }
+    return pokeData
 }
 
-getPokemonData(pokeRef)
+async function displayPokemon() {
+    let data = await getPokemonData(pokeRef)
+    sprite.src = data.sprite;
+}
+
+
+displayPokemon()
+
 //plan
-// write a fetch request to the pokemon api endpoint above
+// write a fetch request to the pokemon api endpoint above ✅
+// display the pokemon sprite on the screen ✅
+// display the pokemon stats on the screen
